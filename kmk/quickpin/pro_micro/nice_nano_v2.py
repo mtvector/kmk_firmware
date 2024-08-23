e2000ba5af8013f2688ef02d390996d04eb50845
mtvector <mt.free.thinking@gmail.com>
2024-08-23 00:56:18 -0700

    add nnv2

---
+++ kmk/quickpin/pro_micro/nice_nano_v2.py
@@ -0,0 +1,32 @@
+import board
+
+pinout = [
+    board.P0_06,#TX/D1
+    board.P0_08,#RX/D0
+    None,  # GND
+    None,  # GND
+    board.P0_17,#SDA/D2
+    board.P0_20,#SCL/D3
+    board.P0_22,#D4
+    board.P0_24,#D5
+    board.P1_00,#D6
+    board.P0_11,#D7
+    board.P1_04,#D8
+    board.P1_06,#D9
+    board.P0_09,#D10
+    board.P0_10,#D16
+    board.P1_11,#D14
+    board.P1_13,#D15
+    board.P1_15,#D18
+    board.P0_02,#D19
+    board.P0_29,#D20
+    board.P0_31,#D21
+    None,  # 3.3v
+    None,  # RST
+    None,  # GND
+    None,  # Battery+
+    board.P1_01,
+    board.P1_02,
+    board.P1_07,
+]
+
