de635ad58100923529646b9568a2680518f89349
mtvector <mt.free.thinking@gmail.com>
2024-08-30 23:47:51 -0700

    comment out bluetooth stuff for now (not useable)

--- boards/choctopus44/main.py
+++ boards/choctopus44/main.py
@@ -22,6 +22,7 @@ encoder_handler = EncoderHandler()
 tapdance = TapDance()
 
 holdtap.tap_time = 250
+holdtap.prefer_hold = False
 
 keyboard.modules = [layers, combos, holdtap, mouse_key,encoder_handler,tapdance]
 keyboard.extensions.append(MediaKeys())
@@ -42,16 +43,23 @@ lmods = [KC.LGUI, KC.LALT, KC.LCTRL, KC.LSFT]
 rmods = [KC.RSFT,KC.RCTRL, KC.RALT, KC.RGUI]
 hrl0 = [KC.A, KC.S, KC.D,KC.F]
 hrr0 = [KC.J,KC.K,KC.L,KC.SCLN]
+hrl1 = [KC.N0, KC.N4, KC.N5, KC.N6]
 hml0 = [KC.HT(k,m) for k,m in zip(hrl0,lmods)]
 hmr0 = [KC.HT(k,m) for k,m in zip(hrr0,rmods)]
-        
+hml1 = [KC.HT(k,m) for k,m in zip(hrl1,rmods)]
+
 combos.combos = [
     Chord((hml0[shft_ind], KC.SPC), KC.UNDERSCORE),
-    Chord((hmr0[-shft_ind], KC.SPC), KC.UNDERSCORE),
+    Chord((hmr0[-(shft_ind+1)], KC.SPC), KC.UNDERSCORE),
     Chord((hml0[ctrl_ind], KC.SPC), KC.TAB),
-    Chord((hmr0[-ctrl_ind], KC.SPC), KC.TAB),
+    Chord((hmr0[-(ctrl_ind+1)], KC.SPC), KC.TAB),
     Chord((hml0[shft_ind], KC.BSPC), KC.DEL),
-    Chord((hmr0[-shft_ind], KC.BSPC), KC.DEL)
+    Chord((hmr0[-(shft_ind+1)], KC.BSPC), KC.DEL),
+    Chord((KC.Z,KC.X),KC.LCTRL(KC.Z)),
+    Chord((KC.X,KC.C),KC.LCTRL(KC.X)),
+    Chord((KC.C,KC.V),KC.LCTRL(KC.C)),
+    Chord((KC.V,KC.B),KC.LCTRL(KC.V)),
+    Chord((KC.C,KC.V,KC.B),KC.LCTRL(KC.LSFT(KC.V))),
 ]
 
 
@@ -65,25 +73,27 @@ keyboard.keymap = [
     [  # default_layer
         KC.ESC, KC.Q, KC.W, KC.E, KC.LT(1, KC.R), KC.T, KC.Y, KC.LT(1, KC.U), KC.I, KC.O, KC.P, KC.BSLASH,
         KC.TAB] + hml0 + [KC.LT(2, KC.G), KC.LT(2, KC.H)] + hmr0 +[ KC.QUOT,
-        KC.EXCLAIM, KC.Z, KC.X, KC.C, KC.V, KC.B, KC.N, KC.M, KC.COMMA, KC.DOT, KC.QUESTION, KC.SLSH,
+        #KC.TILDE,KC.EXCLAIM, KC.Z, KC.X, KC.C, KC.V, KC.B, KC.N, KC.M, KC.COMMA, KC.DOT, KC.SLSH,
+        KC.EXCLAIM, KC.Z, KC.X, KC.C, KC.V, KC.B, KC.N, KC.M, KC.COMMA, KC.DOT, KC.SLSH,KC.TILDE,
         KC.F1, KC.TG(2), KC.SPC, KC.TD(KC.TO(1),KC.TO(2),KC.TO(3)), KC.ENT, KC.BSPC, KC.TG(1), KC.LGUI
     ],
     [  # lower_layer
-        KC.ESC, KC.SLSH, KC.N7, KC.N8, KC.N9, KC.PLUS, KC.F3, KC.F4, KC.UP, KC.F5, KC.F6, KC.F7,
-        KC.CIRC, KC.ASTR, KC.N4, KC.N5, KC.N6, KC.DOT, KC.EQL, KC.LEFT, KC.DOWN, KC.RIGHT, KC.F9, KC.F10,
-        KC.TO(0), KC.N0, KC.N1, KC.N2, KC.N3, KC.MINS, KC.COMMA, KC.HOME, KC.PGDN, KC.END, KC.PGUP, KC.F12,
+        KC.ESC, KC.ASTR, KC.N7, KC.N8, KC.N9, KC.PLUS, KC.F3, KC.F4, KC.UP, KC.F5, KC.F6, KC.F7,
+        KC.CIRC] + hml1+ [KC.DOT, KC.EQL, KC.LEFT, KC.DOWN, KC.RIGHT, KC.F9, KC.F10,
+        KC.TO(0), KC.SLSH, KC.N1, KC.N2, KC.N3, KC.MINS, KC.F2, KC.HOME, KC.PGDN, KC.END, KC.PGUP, KC.F12,
         KC.F1, KC.TG(2), KC.SPC, KC.TD(KC.TO(0),KC.TO(2),KC.TO(3)), KC.ENT, KC.BSPC, KC.TG(1), KC.TO(3)
     ],
     [  # raise_layer
-        KC.ESC, XXXXXXX, XXXXXXX, KC.BSLASH, KC.SLSH, KC.LSFT(KC.N9), KC.EXCLAIM, KC.AMPR, KC.PIPE, KC.BRIGHTNESS_DOWN, KC.BRIGHTNESS_UP, KC.AUDIO_VOL_UP,
-        KC.TAB, KC.CIRC, KC.LCBR, KC.LPRN, KC.LBRC, KC.HASH, KC.TILDE, KC.RBRC, KC.RPRN, KC.RCBR, KC.MINS, KC.AUDIO_VOL_DOWN,
-        KC.TO(0), XXXXXXX, XXXXXXX, KC.PERC, KC.DOLLAR, KC.UNDS, KC.GRAVE, KC.AT, KC.EQL, XXXXXXX, XXXXXXX, KC.AUDIO_MUTE,
+        KC.ESC, KC.GRAVE,XXXXXXX, KC.BSLASH, KC.SLSH, KC.LSFT(KC.N9), KC.EXCLAIM, KC.AMPR, KC.PIPE, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC.TAB, KC.HASH, KC.LCBR, KC.LPRN, KC.LBRC, KC.HASH, KC.CIRC, KC.RBRC, KC.RPRN, KC.RCBR, KC.MINS, KC.EQL,
+        KC.TO(0), XXXXXXX, XXXXXXX, KC.PERC, KC.DOLLAR, KC.UNDS, XXXXXXX, KC.AT, KC.ASTR, XXXXXXX, KC.TILDE, XXXXXXX,
         KC.F1, KC.TG(2), KC.SPC,KC.TD(KC.TO(0),KC.TO(1),KC.TO(3)), KC.ENT, KC.BSPC, KC.TG(1), KC.TO(3)
     ],
     [  # adjust_layer
-        KC.RELOAD, KC.RESET, KC.BOOTLOADER, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC.UP, XXXXXXX, XXXXXXX, XXXXXXX,
-        KC.BLE_REFRESH, KC.BLE_DISCONNECT, KC.HID, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC.LEFT, KC.DOWN, KC.RIGHT, XXXXXXX, XXXXXXX,
-        KC.TO(0), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC.RELOAD, KC.RESET, KC.BOOTLOADER, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC.UP, KC.BRIGHTNESS_DOWN, KC.BRIGHTNESS_UP, KC.AUDIO_VOL_UP,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC.LEFT, KC.DOWN, KC.RIGHT, XXXXXXX, KC.AUDIO_VOL_DOWN,
+        #KC.BLE_REFRESH, KC.BLE_DISCONNECT, KC.HID, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC.LEFT, KC.DOWN, KC.RIGHT, XXXXXXX, KC.AUDIO_VOL_DOWN,
+        KC.TO(0), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC.AUDIO_MUTE,
         _______, KC.TO(0), KC.SPC, KC.TD(KC.TO(0),KC.TO(1),KC.TO(2)), KC.ENT, KC.BSPC, KC.TO(0), _______
     ],
 ]
@@ -97,4 +107,4 @@ encoder_handler.map = [ ((KC.UP, KC.DOWN),), # Layer 1
 # fmt:on
 
 if __name__ == '__main__':
-    keyboard.go(hid_type=HIDModes.USB, secondary_hid_type=HIDModes.BLE, ble_name='choctopus')
+    keyboard.go(hid_type=HIDModes.USB)#, secondary_hid_type=HIDModes.BLE, ble_name='choctopus')
